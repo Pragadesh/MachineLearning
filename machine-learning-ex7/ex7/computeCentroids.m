aa58f3dbfc1e44ea87c28f04c93a6941ca602539
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-03-01 22:12:13 -0800

    completed assignment 2

--- machine-learning-ex7/ex7/computeCentroids.m
+++ machine-learning-ex7/ex7/computeCentroids.m
@@ -26,7 +26,10 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
+for i = 1:K
+  kIdx = (idx == i);
+  centroids(i,:) = (sum(X .* kIdx) / sum(kIdx));
+endfor
 
 
 
