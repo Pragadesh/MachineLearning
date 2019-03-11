55673a49cf7706a551472a1faa8a38e8a21fb117
pgopalakrishnan <pgopalakrishnan@ebay.com>
2019-03-11 09:38:24 -0700

    Completed assignment for week 9

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -8,8 +8,7 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 
 % Unfold the U and W matrices from params
 X = reshape(params(1:num_movies*num_features), num_movies, num_features);
-Theta = reshape(params(num_movies*num_features+1:end), ...
-                num_users, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features);
 
             
 % You need to return the following values correctly
@@ -42,8 +41,13 @@ Theta_grad = zeros(size(Theta));
 
 
 
+cost1 = (X * Theta') .* (R==1);
+cost2 = Y .* (R==1);
+J = sum(sum(((cost1 - cost2) .^ 2))) / 2 + (sum(sum((Theta .^ 2))) * lambda / 2) + (sum(sum((X .^ 2))) * lambda / 2);
 
 
+X_grad = (cost1 - cost2) * Theta + (lambda * X);
+Theta_grad = (cost1 - cost2)' * X + (lambda * Theta);
 
 
 
